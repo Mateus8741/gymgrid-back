aaade6704f9604bf91625ba3ee39d953342e921a
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-06-03 17:01:25 -0300

    chore: Update npm dependencies and server port

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,6 @@ import { fastify } from "fastify";
 
 const app = fastify();
 
-app.listen({ port: 3000, host: "0.0.0.0" }).then(() => {
+app.listen({ port: 3100, host: "0.0.0.0" }).then(() => {
     console.log("Server is running on port 3000");
 });
