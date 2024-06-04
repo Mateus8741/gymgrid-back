c877ec8415292837a683f26b89c12eb3bcfca384
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-06-04 11:12:43 -0300

    chore: Update dependencies and fix code formatting

--- src/routes/registerUser.ts
+++ src/routes/registerUser.ts
@@ -1,50 +1,54 @@
-import bcrypt from "bcryptjs";
-import { FastifyInstance } from "fastify";
-import { ZodTypeProvider } from "fastify-type-provider-zod";
-import { prisma } from "../prisma/prisma-client";
-import { RegisterSchema } from "../schema/RegisterSchema";
+import bcrypt from 'bcryptjs'
+import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { prisma } from '../prisma/prisma-client'
+import { RegisterSchema } from '../schema/RegisterSchema'
 
 export async function registerUser(app: FastifyInstance) {
-    app.withTypeProvider<ZodTypeProvider>().post("/user", {
-        schema: {
-            body: RegisterSchema,
-            summary: "Register a new user",
-            tags: ["User"],
+  app.withTypeProvider<ZodTypeProvider>().post(
+    '/user',
+    {
+      schema: {
+        body: RegisterSchema,
+        summary: 'Register a new user',
+        tags: ['User'],
+      },
+    },
+    async (request, reply) => {
+      try {
+        const { email, password } = request.body
+
+        const alreadyExistsSameEmail = await prisma.user.findFirst({
+          where: {
+            email,
+          },
+        })
+
+        if (alreadyExistsSameEmail) {
+          return reply.status(400).send({
+            message: 'Email already exists',
+          })
         }
-    }, async (request, reply) => {
-        try {
-            const { email, password } = request.body;
 
-            const alreadyExistsSameEmail = await prisma.user.findFirst({
-                where: {
-                    email
-                }
-            });
+        const hashedPassword = await bcrypt.hash(password, 8)
 
-            if (alreadyExistsSameEmail) {
-                return reply.status(400).send({
-                    message: "Email already exists"
-                });
-            }
+        await prisma.user.create({
+          data: {
+            email,
+            password: hashedPassword,
+          },
+        })
 
-            const hashedPassword = await bcrypt.hash(password, 8);
+        return reply.status(201).send({
+          message: 'User registered successfully',
+        })
+      } catch (error) {
+        console.error('Error registering user:', error)
 
-            await prisma.user.create({
-                data: {
-                    email,
-                    password: hashedPassword
-                }
-            });
-
-            return reply.status(201).send({
-                message: "User registered successfully"
-            });
-        } catch (error) {
-            console.error('Error registering user:', error);
-
-            return reply.status(500).send({
-                message: "Internal Server Error"
-            });
-        }
-    });
+        return reply.status(500).send({
+          message: 'Internal Server Error',
+        })
+      }
+    },
+  )
 }
--- src/server.ts
+++ src/server.ts
@@ -1,48 +1,53 @@
-import fastifyCookie from "@fastify/cookie";
-import fastifyCors from "@fastify/cors";
-import fastifyJwt from "@fastify/jwt";
-import fastifySwagger from "@fastify/swagger";
-import fastifySwaggerUi from "@fastify/swagger-ui";
-import { fastify } from "fastify";
-import { ZodTypeProvider, jsonSchemaTransform, serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
-import { loginUser } from "./routes/loginUser";
-import { registerUser } from "./routes/registerUser";
+/* eslint-disable prettier/prettier */
+import fastifyCookie from '@fastify/cookie'
+import fastifyCors from '@fastify/cors'
+import fastifyJwt from '@fastify/jwt'
+import fastifySwagger from '@fastify/swagger'
+import fastifySwaggerUi from '@fastify/swagger-ui'
+import { fastify } from 'fastify'
+import {
+    ZodTypeProvider,
+    jsonSchemaTransform,
+    serializerCompiler,
+    validatorCompiler,
+} from 'fastify-type-provider-zod'
+import { loginUser } from './routes/loginUser'
+import { registerUser } from './routes/registerUser'
 
-
-const app = fastify().withTypeProvider<ZodTypeProvider>();
+const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.register(fastifyCookie)
 app.register(fastifyJwt, { secret: 'supersecret-grid' })
 
 app.register(fastifyCors, {
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
-    allowedHeaders: ["Content-Type"],
-});
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+  allowedHeaders: ['Content-Type'],
+})
 
 app.register(fastifySwagger, {
-    swagger: {
-        consumes: ["application/json"],
-        produces: ["application/json"],
-        info: {
-            title: "GymGrid API",
-            description: "Rotas do GymGrid",
-            version: "1.0.0",
-        },
+  swagger: {
+    consumes: ['application/json'],
+    produces: ['application/json'],
+    info: {
+      title: 'GymGrid API',
+      description: 'Rotas do GymGrid',
+      version: '1.0.0',
     },
-    transform: jsonSchemaTransform,
+  },
+  transform: jsonSchemaTransform,
 })
 
 app.register(fastifySwaggerUi, {
-    routePrefix: "/docs",
+  routePrefix: '/docs',
 })
 
-app.setValidatorCompiler(validatorCompiler);
-app.setSerializerCompiler(serializerCompiler);
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
 
-app.register(registerUser);
-app.register(loginUser);
+app.register(registerUser)
+app.register(loginUser)
 
-app.listen({ port: 3100, host: "0.0.0.0" }).then(() => {
-    console.log("Server is running on port 3100");
-});
+app.listen({ port: 3100, host: '0.0.0.0' }).then(() => {
+  console.log('Server is running on port 3100')
+})
