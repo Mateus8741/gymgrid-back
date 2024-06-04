4b3bee358cbe0cfeab6e1ad27950f44479e9eecb
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-06-04 10:20:58 -0300

    chore: Add loginUser route for user authentication

---
+++ src/routes/loginUser.ts
@@ -0,0 +1,49 @@
+import bcrypt from "bcryptjs";
+import { FastifyInstance } from "fastify";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import { prisma } from "../prisma/prisma-client";
+import { RegisterSchema } from "../schema/RegisterSchema";
+
+export async function loginUser(app: FastifyInstance) {
+    app.withTypeProvider<ZodTypeProvider>().post('/auth/login', {
+        schema: {
+          body: RegisterSchema,
+          summary: "Login a user",
+          tags: ["User"]
+        }
+    }, async (request, reply) => {
+        const { email, password } = request.body
+      
+        const user = await prisma.user.findFirst({
+          where: {
+            email
+          }
+        })
+      
+        if (!user) {
+          reply.status(401)
+          return {
+            error: "Invalid credentials"
+          }
+        }
+      
+        const passwordMatch = await bcrypt.compare(password, user.password)
+      
+        if (!passwordMatch) {
+          reply.status(401)
+          return {
+            error: "Invalid credentials"
+          }
+        }
+      
+        const token = await reply.jwtSign({
+          sign: {
+            sub: user.id
+          }
+        })
+      
+        return reply.send({
+          token
+        })
+      })
+}
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,18 @@
+import fastifyCookie from "@fastify/cookie";
 import fastifyCors from "@fastify/cors";
+import fastifyJwt from "@fastify/jwt";
 import fastifySwagger from "@fastify/swagger";
 import fastifySwaggerUi from "@fastify/swagger-ui";
 import { fastify } from "fastify";
 import { ZodTypeProvider, jsonSchemaTransform, serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
+import { loginUser } from "./routes/loginUser";
 import { registerUser } from "./routes/registerUser";
 
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+app.register(fastifyCookie)
+app.register(fastifyJwt, { secret: 'supersecret-grid' })
 
 app.register(fastifyCors, {
     origin: "*",
@@ -35,6 +41,7 @@ app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
 app.register(registerUser);
+app.register(loginUser);
 
 app.listen({ port: 3100, host: "0.0.0.0" }).then(() => {
     console.log("Server is running on port 3100");
